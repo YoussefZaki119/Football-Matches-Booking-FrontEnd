b0cfa86e26f5fc4f99f9baf3ce4f0ce3ab06bc51
jomana1 <jomana.abdelnaim02@eng-st.cu.edu.eg>
2023-11-22 14:10:19 +0200

    jomanaaa

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,36 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Login from "./Login"
 import Register from "./Register";
+import Card from "./Card.jsx";
+import Matches from "../matches.js";
 
-var IsRegistered = false;
-
+var IsRegistered = true;
+function createMatch(Matches) {
+  return (
+    <Card
+      key={Matches.id}
+      team1={Matches.team1}
+      team2={Matches.team2}
+      date={Matches.date}
+      time={Matches.time}
+      image_url1 ={Matches.image_url1}
+      image_url2 ={Matches.image_url2}
+      venue={Matches.venue}
+      mainRefree={Matches.mainRefree}
+      linesmen={Matches.linesmen}
+     
+    />
+  );
+}
 function App() {
   return (
+    // <div>
+    //   {IsRegistered ? <Login /> : <Register />}
+    // </div>
     <div>
-      {IsRegistered ? <Login /> : <Register />}
-    </div>
+      {Matches.map(createMatch)}
+      
+</div>
   );
 }
 
---
+++ src/teams.js
@@ -0,0 +1,95 @@
+
+const Teams= [
+    {
+      "id": 1,
+      "name": "Al Ahly SC",
+      "image_url": "https://example.com/al_ahly_logo.png"
+    },
+    {
+      "id": 2,
+      "name": "Zamalek SC",
+      "image_url": "https://example.com/zamalek_logo.png"
+    },
+    {
+      "id": 3,
+      "name": "Ismaily SC",
+      "image_url": "https://example.com/ismaily_logo.png"
+    },
+    {
+      "id": 4,
+      "name": "Al Masry SC",
+      "image_url": "https://example.com/al_masry_logo.png"
+    },
+    {
+      "id": 5,
+      "name": "Pyramids FC",
+      "image_url": "https://example.com/pyramids_logo.png"
+    },
+    {
+      "id": 6,
+      "name": "El Mokawloon SC",
+      "image_url": "https://example.com/el_mokawloon_logo.png"
+    },
+    {
+      "id": 7,
+      "name": "ENPPI SC",
+      "image_url": "https://example.com/enppi_logo.png"
+    },
+    {
+      "id": 8,
+      "name": "Tala'ea El Gaish SC",
+      "image_url": "https://example.com/talaea_el_gaish_logo.png"
+    },
+    {
+      "id": 9,
+      "name": "Wadi Degla SC",
+      "image_url": "https://example.com/wadi_degla_logo.png"
+    },
+    {
+      "id": 10,
+      "name": "Al Ittihad Alexandria Club",
+      "image_url": "https://example.com/al_ittihad_logo.png"
+    },
+    {
+      "id": 11,
+      "name": "Ismaily SC",
+      "image_url": "https://example.com/ismaily_logo.png"
+    },
+    {
+      "id": 12,
+      "name": "Smouha SC",
+      "image_url": "https://example.com/smouha_logo.png"
+    },
+    {
+      "id": 13,
+      "name": "Misr Lel-Makkasa SC",
+      "image_url": "https://example.com/misr_lel_makkasa_logo.png"
+    },
+    {
+      "id": 14,
+      "name": "El Entag El Harby SC",
+      "image_url": "https://example.com/el_entag_el_harby_logo.png"
+    },
+    {
+      "id": 15,
+      "name": "Aswan SC",
+      "image_url": "https://example.com/aswan_logo.png"
+    },
+    {
+      "id": 16,
+      "name": "Ceramica Cleopatra FC",
+      "image_url": "https://example.com/ceramica_cleopatra_logo.png"
+    },
+    {
+      "id": 17,
+      "name": "Gouna FC",
+      "image_url": "https://example.com/gouna_logo.png"
+    },
+    {
+      "id": 18,
+      "name": "National Bank of Egypt SC",
+      "image_url": "https://example.com/national_bank_of_egypt_logo.png"
+    }
+  ]
+
+export default Teams;
